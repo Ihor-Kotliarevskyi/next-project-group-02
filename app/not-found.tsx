9bc017be1841604f08d33bcbebaa6101bc176bb5
Ihor Kotliarevskyi <jagor0707@gmail.com>
2026-03-26 07:31:30 +0200

    add start project structure

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+function NotFound() {
+  return (
+    <div>
+      <h1 className="not-found">404 - Page not found</h1>
+      <p className="not-found">
+        Sorry, the page you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
+
+export default NotFound;
