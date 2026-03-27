0f4b983d5b70e736cfa31b56da7ad9ae3a91c2d4
Ihor Kotliarevskyi <jagor0707@gmail.com>
2026-03-27 21:23:47 +0200

    Update react-rating-stars-component.d.ts

--- react-rating-stars-component.d.ts
+++ react-rating-stars-component.d.ts
@@ -1,1 +1,16 @@
-declare module "react-rating-stars-component";
+declare module "react-rating-stars-component" {
+  import React from "react";
+
+  export interface ReactStarsProps {
+    count?: number;
+    value?: number;
+    isHalf?: boolean;
+    edit?: boolean;
+    size?: number;
+    activeColor?: string;
+    // Add other props as needed
+  }
+
+  const ReactStars: React.FC<ReactStarsProps>;
+  export default ReactStars;
+}
