15fb1896a1597c61b16751b058ee6192fd8c0fcd
OlenaPetrova <petrovahel@icloud.com>
2026-04-02 22:10:20 +0300

    Cloundinary location form (#41)
    * my local changes
    
    * add cloundinary
    
    * delete wrong path
    
    * fix edit location editing, move validation and image upload to separate files

--- app/(private routes)/locations/edit/[id]/page.tsx
+++ app/(private routes)/locations/edit/[id]/page.tsx
@@ -2,20 +2,21 @@ import LocationForm from "@/components/LocationForm/LocationForm";
 import { getRegionsServer, getLocationTypesServer, getLocationByIdServer } from "@/lib/api/serverApi";
 
 type Props = {
-  params: Promise<{ locationId: string }>;
+  params: Promise<{ id: string }>;
 };
 
 export default async function Page({ params }: Props) {
-  const { locationId } = await params;
+  const { id } = await params;
+
   const [regions, types, location] = await Promise.all([
     getRegionsServer(),
     getLocationTypesServer(),
-    getLocationByIdServer(locationId),
+    getLocationByIdServer(id),
   ]);
 
   return (
     <LocationForm
-      id={locationId}
+      id={id}
       initialData={location}
       regions={regions}
       locationTypes={types}
