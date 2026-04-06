0183e5e5342a424a67b2f31bdc7ed336749a841f
Inna Melnychenko <dovgopolaii@gmail.com>
2026-04-06 20:53:55 +0200

    delete hr element, add border .copy

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -13,7 +13,11 @@ export default function Footer() {
     <footer className={css.footer}>
       <div className={css.footerContainer}>
         <div className={css.inner}>
-          <button onClick={handleScrollToTop} className={css.logoWrapper} aria-label="Scroll to top">
+          <button
+            onClick={handleScrollToTop}
+            className={css.logoWrapper}
+            aria-label="Scroll to top"
+          >
             <Logo />
           </button>
           <ul className={css.socials}>
@@ -76,7 +80,6 @@ export default function Footer() {
             </li>
           </ul>
         </div>
-        <hr className={css.line} />
         <div className={css.copy}>
           <p>
             &copy; {new Date().getFullYear()} Природні Мандри. Усі права
